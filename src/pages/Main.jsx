c01733f38237f3130db368d14fc30b1230c9a489
zionimo <zionimobu@gmail.com>
2023-02-15 18:02:33 +0900

    카카오 책 검색 REST API 연습

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,42 @@
+import Input from "@mui/joy/Input";
+import Button from "@mui/joy/Button";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+
+import AxiosGet from "../config";
+
 const Main = () => {
-  return <div>메인</div>;
+  return (
+    <div>
+      <div>
+        <Input type="text" placeholder="도서명을 입력하세요!" />
+        <Button>
+          <SearchOutlinedIcon />
+        </Button>
+      </div>
+
+      {/* 도서 출력공간 */}
+      <div>
+        <AxiosGet />
+      </div>
+
+      <div>
+        {/* {object.values(bookData).map((book, index) => (
+          <div key={index}>
+            <span>{book.title}</span>
+          </div>
+        ))} */}
+      </div>
+
+      <div>
+        {/* 이달의 책 */}
+        <section></section>
+        {/* 이달의 문구 */}
+        <section></section>
+      </div>
+
+      <div></div>
+    </div>
+  );
 };
 
 export default Main;
